56592cc99f88883fc3d3838ef85704b3343ede6d
joaocastro <joaocastro0429@gmail.com>
2024-07-09 22:49:58 -0300

    criando um mapeamento de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("Hello world")
+import fastify from "fastify";
+
+const app=fastify()
+
+app.get("/teste",()=>{
+    return "Hello world"
+})
+
+
+
+
+app.listen({port:3333}).then(()=>{
+    console.log("server running")
+})
